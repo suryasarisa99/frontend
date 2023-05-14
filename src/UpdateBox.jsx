020a819f20ed266d18eac89c045e3983489d1647
Jaya Surya <suryasarisa99@gmail.com>
2023-05-14 08:59:43 +0000

    updated

--- src/UpdateBox.jsx
+++ src/UpdateBox.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { useEffect } from "react";
-
+import { GiCrossedSwords } from "react-icons/gi";
 export default function UpdateBox({ updateName, setUpdateBox }) {
   useEffect(() => {
     window.addEventListener("scroll", onScrollHandle);
@@ -12,13 +12,20 @@ export default function UpdateBox({ updateName, setUpdateBox }) {
     };
   }, []);
   let onScrollHandle = () => {
-    console.log("scrolled");
     document.getElementById("overlay").style.display = "none";
     setUpdateBox(false);
   };
   return ReactDOM.createPortal(
     <>
       <form action="" className="update-box" onSubmit={updateName}>
+        <div className="icon-box">
+          <GiCrossedSwords
+            className="close-btn"
+            fill="red"
+            onClick={onScrollHandle}
+          />
+          {/* <FaTimes className="close-btn" fill="blue" /> */}
+        </div>
         <input type="text" placeholder="First Name" name="fname" />
         <input type="text" placeholder="Second Name" name="sname" />
         <input type="text" placeholder="Last Name" name="lname" />
