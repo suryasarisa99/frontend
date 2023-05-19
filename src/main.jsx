153bb8225449e22d4e8d1c117c1676d487268426
Jaya Surya <suryasarisa99@gmail.com>
2023-05-19 14:41:13 +0000

    PWA support

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RegisterProvider } from "../context/registerId";
+import { registerSW } from "virtual:pwa-register";
 
+registerSW();
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <>
     <RegisterProvider>
