b8246f4535145921e2cdb5d063ec4615dd828085
jaya surya <suryasarisa99.com>
2023-07-22 12:05:52 +0000

    jwt

--- src/components/SidePannel.jsx
+++ src/components/SidePannel.jsx
@@ -117,8 +117,6 @@ const SidePannel = forwardRef((props, ref) => {
         >
           other site
         </motion.a>
-
-        <button onClick={() => navigate("/login")}>login</button>
       </motion.div>
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     port: 3000,
     host: "192.168.0.169",
